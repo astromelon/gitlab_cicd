578f9b7db62d7af1493fb3ed09455e004741957c
Takgon Lee <astromelon@gmail.com>
2024-01-09 15:56:43 +0900

    2024-01-09 최초 커밋
    최초 커밋

---
+++ HashiCorp/gitlab-ce/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["ffi",[">= 1.0.0"]],["win32-process",[">= 0"]],["vagrant-sshfs",["= 1.3.6"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.21.0"]]],"checksum":"9f328802801d47858ef65453425ba9947f1ee7f8dd6006dc02b454a54e5e501d","vagrant_version":"2.2.18"}
